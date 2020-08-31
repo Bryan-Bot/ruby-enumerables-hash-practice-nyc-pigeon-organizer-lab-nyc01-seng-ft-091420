d5e29197778cd9e92d778a23948fd1d1ffdfa5d8
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-31 19:55:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value), pigeon_list|
+    value.each do |color, names|
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][key]
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key].push(color.to_s)
+      end
+    end
+  end
 end
