5c5bf9e25b32104c8921628ab97fff9da08a470d
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-31 19:39:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,9 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  binding.pry
+  data.each_with_object({}) do |(key, value), pigeon_list|
+    value.each do |color, names|
+      binding.pry
+    end
+  end
 end
